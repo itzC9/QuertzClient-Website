efce8971df149de6d5195207341fe2d09d0a96e9
itzC9 <154809425+itzC9@users.noreply.github.com>
2025-06-20 15:39:13 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,6 @@ app.config["DISCORD_OAUTH_CLIENT_ID"] = "1257710925662785596"
 app.config["DISCORD_OAUTH_CLIENT_SECRET"] = "fWxV0t5GMEmGywzg1zp_fDfPuE7gFq_a"
 app.config["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 discord_bp = make_discord_blueprint(scope=["identify", "email"], redirect_url="/discord_callback")
-app.register_blueprint(discord_bp, url_prefix="/auth")
+app.register_blueprint(discord_bp, url_prefix="/login")
 
 from app import routes
